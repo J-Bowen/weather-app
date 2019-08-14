b3afe0b1bf3577582c9de1d8178425f168cfac56
Jack Bowen <jack.bowen@artefact.com>
2019-08-14 09:15:50 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('main.html.j2')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    a_website = "http://127.0.0.1:5000/"
+    #webbrowser.get('chrome').open_new(a_website)
+
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+# driver = webdriver.Chrome()
+# driver.get("http://www.python.org")
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
---
+++ static/myChart.js
@@ -0,0 +1,148 @@
+document.addEventListener('DOMContentLoaded', function() {
+    getData('London,uk');
+    getData('Paris,fr');
+    getData('Berlin,de');
+    getData('Copenhagen,dk');
+}, false);
+
+
+var data=[];
+var labels=[];
+var humidity=[];
+
+function renderChart() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var ctx2 = document.getElementById("myChart2").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                fill: 	true,
+                fillColor: '#F0FFFF',
+                borderColor: "#6495ED",
+                backgroundColor: "#e755ba",
+                pointDisplay: false,
+                pointHoverBackgroundColor: "#55bae7",
+                pointHoverBorderColor: "#55bae7",
+                data: data,
+                  
+            }],
+        },
+        options: {
+            elements: { point: { radius: 5 } },
+
+            legend: {
+                display: false,
+            },
+
+            scaleFontColor: 'red',
+            responsive: true,
+            tooltips: {
+                mode: 'single',
+            },
+            scales: {
+                display: false,
+                xAxes: [{ 
+                    gridLines: {
+                        display: false,
+                        color: "#FFFFFF"
+                    },
+                    ticks: {
+                        display: false,
+                        fontColor: "#FFFFFF", // this here
+                    },
+                }],
+                yAxes: [{
+                    display: false,
+                    gridLines: {
+                        display: false,
+                        color: "#FFFFFF",
+                    },
+                }],
+            }
+        }         
+    });
+    var myChart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                fill: 	true,
+                fillColor: '#F0FFFF',
+                borderColor: "#6495ED",
+                backgroundColor: "#e755ba",
+                pointDisplay: false,
+                pointHoverBackgroundColor: "#55bae7",
+                pointHoverBorderColor: "#55bae7",
+                data: humidity,
+                  
+            }],
+        },
+        options: {
+            elements: { point: { radius: 5 } },
+
+            legend: {
+                display: false,
+            },
+
+            scaleFontColor: 'red',
+            responsive: true,
+            tooltips: {
+                mode: 'single',
+            },
+            scales: {
+                display: false,
+                xAxes: [{ 
+                    gridLines: {
+                        display: false,
+                        color: "#FFFFFF"
+                    },
+                    ticks: {
+                        display: false,
+                        fontColor: "#FFFFFF", // this here
+                    },
+                }],
+                yAxes: [{
+                    display: false,
+                    gridLines: {
+                        display: false,
+                        color: "#FFFFFF",
+                    },
+                }],
+            }
+        }         
+    });
+}
+
+function getData(id){
+    var request = new XMLHttpRequest();
+    //check city ID, guessing Lon ...
+    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q='+id+'&APPID=86ccbefb8db547e08c6be822900d77a1', true);
+    request.onload = function () {
+        // Begin accessing JSON data here
+        data.push(JSON.parse(this.response).main.temp);
+        humidity.push(JSON.parse(this.response).main.humidity);
+        labels.push(id);
+        plotGraph();
+        
+    }
+    request.send();
+}
+
+function updateAverageTemp(){
+    var avgg;
+
+    for(var i=0; i<data.length; i++){
+        avgg += data[i];
+    }
+    avgg/=4;
+    document.getElementById('avg').innerHTML = Math.round((data[0]+data[1]+data[2]+data[3])/4)+'K';
+    document.getElementById('avg2').innerHTML = Math.round((humidity[0]+humidity[1]+humidity[2]+humidity[3])/4)+'%';
+}
+function plotGraph(){
+    renderChart(data, labels);
+    if(data.length>0){
+        updateAverageTemp();
+    }
+}
