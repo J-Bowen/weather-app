9be0f583262a4a40e58e58404434b3bbd6f30a1b
J-Bowen <jack.bowen@artefact.com>
2019-08-30 12:23:22 +0100

    redis

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ import redis
 
 app = Flask(__name__)
 
-# redis_host = os.environ.get('REDISHOST', 'localhost')
-# redis_port = int(os.environ.get('REDISPORT', 6379))
-# redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
+redis_host = os.environ.get('REDISHOST', 'localhost')
+redis_port = int(os.environ.get('REDISPORT', 6379))
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
 
 
 def setDataInRedis():
-    # value = redis_client.incr('counter', 1)
-    # return 'Visitor number: {}'.format(value)
-    return None
-    
+    value = redis_client.incr('counter', 1)
+    return 'Visitor number: {}'.format(value)
+    # return None
+
 def getDataFromRedis():
 
     return None
